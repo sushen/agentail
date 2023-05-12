5c0c5d17ea75f3da0bcdf81b0128f6c1592121d0
Sushen Biswas <sushenbiswasaga@gmail.com>
2023-05-12 10:44:34 +0600

    Delete

---
+++ login/login_contituency.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+from selenium.webdriver.common.by import By
+
+from driver.driver import Driver
+
+from dotenv import load_dotenv
+
+# Load the environment variables
+load_dotenv()
+
+
+class Login:
+    def login(self, driver):
+        # print(input("Inside class and function :"))
+        try:
+            # I use environment variable  base on this tutorials https://www.youtube.com/watch?v=IolxqkL7cD8
+            FACEBOOK_USERNAME = os.getenv("contituency_USERNAME", "")
+            FACEBOOK_PASSWORD = os.getenv("contituency_PASSWORD", "")
+            # FACEBOOK_USERNAME = os.environ.get('facebook_email')
+            # FACEBOOK_PASSWORD = os.environ.get('facebook_pass')
+            # print(FACEBOOK_PASSWORD)
+            # print(input("...:"))
+            driver.find_element(By.NAME, "email").send_keys(FACEBOOK_USERNAME)
+            driver.find_element(By.NAME, "password").send_keys(FACEBOOK_PASSWORD)
+            time.sleep(2)
+            driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+
+            # print(input("Login work Successfully Press any Key: "))
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    driver = Driver().driver
+    driver.get("https://www.facebook.com/")
+    Login().login(driver)
